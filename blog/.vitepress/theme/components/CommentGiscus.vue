fbe9ae5fc7f1ec382344180ed9e7c42237b2f12a
Gau, Syu <GauSyu@users.noreply.github.com>
2026-05-19 20:53:23 +0800

    Update CommentGiscus.vue

--- blog/.vitepress/theme/components/CommentGiscus.vue
+++ blog/.vitepress/theme/components/CommentGiscus.vue
@@ -1,30 +1,3 @@
 <template>
-    <div id="giscus-container"></div>
+  <!-- 彻底移除评论渲染 -->
 </template>
-
-<script setup>
-import giscusTalk from 'vitepress-plugin-comment-with-giscus'
-import { useData, useRoute } from 'vitepress'
-
-const { frontmatter, theme } = useData()
-const route = useRoute()
-let { repo, repoId, categoryId } = theme.value.comment
-giscusTalk(
-    {
-        repo: repo,
-        repoId: repoId,
-        category: 'General',
-        categoryId: categoryId,
-        mapping: 'pathname',
-        inputPosition: 'top',
-        lang: 'en',
-        lightTheme: 'light',
-        darkTheme: 'transparent_dark'
-    },
-    {
-        frontmatter,
-        route
-    },
-    true
-)
-</script>
